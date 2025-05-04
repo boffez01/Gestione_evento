f3cea675c8a73c740484171c8f699a4b6fb657b0
harshpreet singh <harshsingh.hs.389.hs@gmail.com>
2025-05-04 17:12:48 +0200

    Initial commit

---
+++ src/test/java/it/epicode/Gestione_evento/GestioneEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Gestione_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
